85e90bc08946fd65e378cf49a5e98089278d874b
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-02-13 04:12:45 -0800

    Completed Part 3. Implemented code for controlling the player.
    Based on Official Unity Documentation Tutorials. Link: https://unity3d.com/learn/tutorials/projects/2d-ufo-tutorial/controlling-player?playlist=25844
    
    - Learned how to add a RigidBody component to a game object and reference it on a script.
    - Learned how to apply forces to the rigid bodies via the Unity C# API.
    - P.S. Refactored the horrible names for some variables (from a physics standpoint...) proposed in the tutorials...
    - Learned how to work with vectors - their directions and their magnitudes.
    
    It had been a while since I coded in C#. What a nice, neat and tidy language.

---
+++ 2D-UFO-Project-Tutorial/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    // FIRST THINGS FIRST
+    private Rigidbody2D Rb2dReference; // A reference to a component in the same game object.
+    public float ForceMagnitude; // More precise name...
+
+    void Start ()
+    {
+        Rb2dReference = GetComponent<Rigidbody2D>();
+        //ForceVectorMagnitude = 25; Unity editor actually adds a field automatically for this
+    }
+
+
+
+	// We wanna check every frame for user input.
+	// And then apply that input to move our game object (the player).
+	// FOR CHECKING AND APPLY THIS INPUT - THERE ARE TWO CHOICES.
+	// Update() - Called before rendering a frame. (Most code go here cause most things happen here)
+	// FixedUpdate() - Called just before performing any physics calculations. (Physics code goes here)
+
+
+	// CODE FOR PHYSICS
+	void FixedUpdate()
+	{
+        // Unity documentations seems to be pretty good...
+        // This input will be used for applying forces to the rigid body
+        float MoveHorizontal = Input.GetAxis("Horizontal"); // InputManager does a great job recording automatically from the keys at the keybord. 
+		float MoveVertical = Input.GetAxis("Vertical");
+
+        // Actual interaction with the physics engine.
+        Vector2 ForceVector = new Vector2(MoveHorizontal, MoveVertical) * ForceMagnitude;
+        Rb2dReference.AddForce(ForceVector);
+        
+	}
+
+	
+}
+
