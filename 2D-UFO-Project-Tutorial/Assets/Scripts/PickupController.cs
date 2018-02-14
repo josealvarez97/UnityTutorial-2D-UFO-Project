59b3b83089d0260eb0bcb6f192fccbcf6f607871
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-02-13 23:39:18 -0800

    Added collectable objects and implemented script for their rotation with Euler's angles
    Based on official Unity documentation Tutorials. Link: https://docs.unity3d.com/Manual/ControllingGameObjectsComponents.html
    
    - Learned to make a rigid body rotat with Euler's angle (Useful link: https://www.youtube.com/watch?v=q0jgqeS_ACM)
    - Learned about prefabs. Important concept!

---
+++ 2D-UFO-Project-Tutorial/Assets/Scripts/PickupController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupController : MonoBehaviour {
+
+
+	void Update () {
+
+        // Good link to understand rotations with Euler's angles.
+        //https://www.youtube.com/watch?v=q0jgqeS_ACM
+        transform.Rotate(new Vector3(0, 0, 45) * Time.deltaTime);		
+	}
+}
