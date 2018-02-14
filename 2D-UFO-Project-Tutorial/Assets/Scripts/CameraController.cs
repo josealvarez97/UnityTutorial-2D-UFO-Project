8bdab36b20353cf3b3d6721e3ef301bbdd019fae
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-02-13 20:15:21 -0800

    Implemented camera movement.
    Based on the official Unity documentation tutorials. Link: https://unity3d.com/learn/tutorials/projects/2d-ufo-tutorial/following-player-camera?playlist=25844
    
    - Learned how to reference a game object in a script. (Useful documentation: https://docs.unity3d.com/Manual/ControllingGameObjectsComponents.html)
    - Learned how to work with 3 dimensional vectors to adjust the transform component of two game objects in a way that one follows the other.
    - Learned more about the implications of the position of the camera.

---
+++ 2D-UFO-Project-Tutorial/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    // I need a reference to the player
+    public GameObject PlayerReference;
+
+    private Vector3 CameraOffsetFromPlayer;
+    
+
+	// Use this for initialization
+	void Start () {
+        //PlayerReference = GetComponent<GameObject>(); Does not work because it is not a component of this object.
+
+        CameraOffsetFromPlayer = PlayerReference.transform.position - transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+
+        // No idea why the code below does not work...
+        // transform.position.Set(PlayerReference.transform.position.x, PlayerReference.transform.position.y,transform.position.z);
+
+        transform.position = PlayerReference.transform.position - CameraOffsetFromPlayer;
+
+    }
+
+}
