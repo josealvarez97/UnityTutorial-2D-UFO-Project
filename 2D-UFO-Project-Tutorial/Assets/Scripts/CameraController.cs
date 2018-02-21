45fbe3c2274c4813a82d9ff69bfcaa9ffc7d6a37
Jose Alvarez C <alvarez.jo.developer@gmail.com>
2018-02-20 18:22:37 -0800

    Implemented Pinch To Zoom interaction (Zoom)
    A bit buggy... works but there are times in which it zooms more than it should even though it is validated for not doing so...
    Useful documentation:
    - Pinch To Zoom (https://unity3d.com/learn/tutorials/topics/mobile-touch/pinch-zoom?playlist=17138)

--- 2D-UFO-Project-Tutorial/Assets/Scripts/CameraController.cs
+++ 2D-UFO-Project-Tutorial/Assets/Scripts/CameraController.cs
@@ -8,17 +8,74 @@ public class CameraController : MonoBehaviour {
     public GameObject PlayerReference;
 
     private Vector3 CameraOffsetFromPlayer;
-    
 
-	// Use this for initialization
-	void Start () {
+
+    // Pinch to Zoom Code
+    public float perspectiveZoomSpeed = .2f;
+    public float orthographicZoomSpeed = .2f;
+
+    private Camera cameraReference;
+
+
+    // Use this for initialization
+    void Start () {
         //PlayerReference = GetComponent<GameObject>(); Does not work because it is not a component of this object.
 
         CameraOffsetFromPlayer = PlayerReference.transform.position - transform.position;
-	}
-	
-	// Update is called once per frame
-	void LateUpdate () {
+
+        cameraReference = GetComponent<Camera>();
+        
+    }
+
+
+    void Update()
+    {
+
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0);
+            Touch touchOne = Input.GetTouch(1);
+
+            Vector2 prevTouchZeroPosition = touchZero.position - touchZero.deltaPosition;
+            Vector2 prevTouchOnePosition = touchOne.position - touchOne.deltaPosition;
+
+
+            float touchesDistance = (touchZero.position - touchOne.position).magnitude;
+            float prevTouchesDistance = (prevTouchZeroPosition - prevTouchOnePosition).magnitude;
+
+            //float pinchToZoomMagnitude = touchesDistance - prevTouchesDistance;
+            // greater distance than before, zoom in.
+            // less distance than before, zoom out
+
+
+            // It's better in the following way.
+            float pinchToZooomMagnitude = prevTouchesDistance - touchesDistance;
+            // greater than before, negative result. Zoom in. (Reduce the field of view or the size of the camera).
+            // less than before, positive result. Zoom out. (Increment field of view or the size of the camera).
+            // Since we want to reduce the field of view or the size of the camera, this is the correct way to substract the two values.
+
+
+
+            if (cameraReference.orthographic)
+            {
+                float newSize = cameraReference.orthographicSize + pinchToZooomMagnitude * orthographicZoomSpeed; // is speed the right wording here..? Maybe it should be...
+                cameraReference.orthographicSize = Mathf.Max(newSize, 50f); // we don't want negatives. Shouldn't we clamp here as well?
+
+
+
+            }
+            else // is not orthographic
+            {
+                cameraReference.fieldOfView += pinchToZooomMagnitude * perspectiveZoomSpeed; // what if the numbers are off...?
+                cameraReference.fieldOfView = Mathf.Clamp(cameraReference.fieldOfView, 50f, 179.9f);
+
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void LateUpdate () {
 
         // No idea why the code below does not work...
         // transform.position.Set(PlayerReference.transform.position.x, PlayerReference.transform.position.y,transform.position.z);
---
+++ 2D-UFO-Project-Tutorial/Assets/Scripts/PinchToZoom.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinchToZoom : MonoBehaviour {
+
+
+    // BETTER TO HAVE THIS CODE IN CAMERA CONTROLLER
+    // will keep it here and not delete the file to remind me of that...
+
+ //   public float perspectiveZoomSpeed = .5f;
+ //   public float orthographicZoomSpeed = .5f;
+
+ //   private Camera cameraReference;
+	//// Use this for initialization
+	//void Start()
+ //   {
+ //       cameraReference = GetComponent<Camera>();
+ //   }
+
+ //   // Update is called once per frame
+ //   void Update () {
+		
+ //       if (Input.touchCount == 2)
+ //       {
+ //           Touch touchZero = Input.GetTouch(0);
+ //           Touch touchOne = Input.GetTouch(1);
+
+ //           Vector2 prevTouchZeroPosition = touchZero.position - touchZero.deltaPosition;
+ //           Vector2 prevTouchOnePosition = touchOne.position - touchOne.deltaPosition;
+
+
+ //           float touchesDistance = (touchZero.position - touchOne.position).magnitude;
+ //           float prevTouchesDistance = (prevTouchZeroPosition - prevTouchOnePosition).magnitude;
+
+ //           //float pinchToZoomMagnitude = touchesDistance - prevTouchesDistance;
+ //           // greater distance than before, zoom in.
+ //           // less distance than before, zoom out
+
+
+ //           // It's better in the following way.
+ //           float pinchToZooomMagnitude = prevTouchesDistance - touchesDistance;
+ //           // greater than before, negative result. Zoom in. (Reduce the field of view or the size of the camera).
+ //           // less than before, positive result. Zoom out. (Increment field of view or the size of the camera).
+ //           // Since we want to reduce the field of view or the size of the camera, this is the correct way to substract the two values.
+
+
+            
+ //           if (cameraReference.orthographic)
+ //           {
+ //               cameraReference.orthographicSize += pinchToZooomMagnitude * orthographicZoomSpeed; // is speed the right wording here..? Maybe it should be...
+ //               cameraReference.orthographicSize = Mathf.Max(cameraReference.orthographicSize, .1f); // we don't want negatives. Shouldn't we clamp here as well?
+
+ //           }
+ //           else // is not orthographic
+ //           {
+ //               cameraReference.fieldOfView += pinchToZooomMagnitude * perspectiveZoomSpeed; // what if the numbers are off...?
+ //               cameraReference.fieldOfView = Mathf.Clamp(cameraReference.fieldOfView, .1f, 179.9f);
+
+ //           }
+
+
+            
+
+ //       }
+
+	//}
+}
